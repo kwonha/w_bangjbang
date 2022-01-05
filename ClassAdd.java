0ba49f3c4383f478fd58bb789aff6f2ee5500195
kwonha <84116360+kwonha@users.noreply.github.com>
2022-01-05 15:47:30 +0900

    Add 완성

---
+++ ClassAdd.java
@@ -0,0 +1,31 @@
+package test.class2;
+
+public class ClassAdd {
+	private int n1, n2, result;
+
+	public int getN1() {
+		return n1;
+	}//gn1
+
+	public void setN1(int n1) {
+		this.n1 = n1;
+	}//sn1
+
+	public int getN2() {
+		return n2;
+	}//gn2
+
+	public void setN2(int n2) {
+		this.n2 = n2;
+	}//sn2
+
+	public int getResult() {
+		return result;
+	}//gR
+	
+	public void calcAdd() {
+		result = n1 + n2;
+	}//calcAdd
+	
+	
+}//ClassAdd
