34305b5bc4851b1336fab564c017f903eef1665a
kwonha <84116360+kwonha@users.noreply.github.com>
2022-01-12 15:46:45 +0900

    constructorcall1

--- ClsA.java
+++ ClsA.java
@@ -1,9 +1,15 @@
-package test.constructorcall1;
+package test.constructorcall2;
 
 public class ClsA {
 	
-	public ClsA(){
-//		super();
-		System.out.println("ClasA() 积己磊 角青凳");
-	}//积
-}//努
+	public ClsA() {
+		System.out.println("ClsA() 扁夯 积己磊 角青");
+	}//积己磊
+	
+	public ClsA(int n) {
+		System.out.println("ClsA(int n) 积己磊 角青 " + n);
+	}//积己磊
+
+
+
+}
--- ClsB.java
+++ ClsB.java
@@ -1,8 +1,15 @@
-package test.constructorcall1;
+package test.constructorcall2;
 
 public class ClsB extends ClsA {
+	
 	public ClsB() {
-//		super();
-		System.out.println("ClsB() 积己磊 角青凳");
-	}//积
-}//努
+		System.out.println("ClsB() 扁夯 积己磊 角青");
+	}//积己磊
+	
+	public ClsB(int n) {
+//		super(); 积帆 蹬乐澜
+		super(n);
+		System.out.println("ClsB(int n) 积己磊 角青 " + n);
+	}//积己磊
+
+}
